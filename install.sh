1c6a0b3f69a621a0299799c6ffdbbe7d2fefa415
wally yang <wallyyang28@gmail.com>
2017-01-08 15:34:04 -0500

    Add install shell script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.bashrc ~/
+ln -s ~/dotfiles/.vimrc ~/
+ln -s ~/dotfiles/.spacemacs.d ~/
+ln -s ~/dotfiles/.ycm_extra_conf.py ~/
+ln -s ~/dotfiles/.xinitrc ~/
+ln -s ~/dotfiles/.zshrc ~/
