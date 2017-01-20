948674ba4c35743766d2a7d41e7c5f7a2e76dc0d
wally yang <wallyyang28@gmail.com>
2017-01-20 09:09:37 -0500

    Remove xinit

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ ln -s ~/dotfiles/.bashrc ~/
 ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.spacemacs.d ~/
 ln -s ~/dotfiles/.ycm_extra_conf.py ~/
-ln -s ~/dotfiles/.xinitrc ~/
 ln -s ~/dotfiles/.zshrc ~/
