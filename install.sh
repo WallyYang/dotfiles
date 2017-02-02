97800525d1f25cc2635dd8832fec0719837188eb
wally yang <wallyyang28@gmail.com>
2017-02-02 15:03:52 -0500

    add oh-my-zsh build in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# create dotfiles config
 ln -s ~/dotfiles/.bashrc ~/
 ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.spacemacs.d ~/
 ln -s ~/dotfiles/.ycm_extra_conf.py ~/
-ln -s ~/dotfiles/.zshrc ~/
 ln -s ~/dotfiles/.i3 ~/
+
+# build oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+ln -s ~/dotfiles/.zshrc ~/
