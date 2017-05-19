4c9f1651848c1d19a0d77a35b64270c82ce3ed2a
Wally Yang <wallyyang28@gmail.com>
2017-05-19 14:03:31 -0400

    separate oh-my-zsh install script

--- install.sh
+++ install.sh
@@ -8,12 +8,9 @@ ln -s ~/dotfiles/.eclimrc ~/
 ln -s ~/dotfiles/.ycm_extra_conf.py ~/
 ln -s ~/dotfiles/.i3 ~/
 
-# build oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-rm ~/.zshrc
-ln -s ~/dotfiles/.zshrc ~/
-
 # create temporary trash can
 sudo mkdir /delete
 sudo chmod 777 /delete
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
---
+++ install2.sh
@@ -0,0 +1,5 @@
+# build oh-my-zsh
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+rm ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~/
+
