0c570d68699a42ecc50403e57a070e5446457f7e
Wally Yang <wallyyang28@gmail.com>
2018-05-21 10:12:58 +0000

    feat: enable check for update

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -104,7 +104,7 @@ values."
    ;; when the current branch is not `develop'. Note that checking for
    ;; new versions works via git commands, thus it calls GitHub services
    ;; whenever you start Emacs. (default nil)
-   dotspacemacs-check-for-update nil
+   dotspacemacs-check-for-update t
    ;; If non-nil, a form that evaluates to a package directory. For example, to
    ;; use different package directories for different Emacs versions, set this
    ;; to `emacs-version'.
@@ -314,7 +314,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
-  (setq ycmd-server-command '("python" "/opt/ycmd/ycmd"))
+  (setq ycmd-server-command '("python" "/home/wally/opt/ycmd/ycmd"))
   )
 
 (defun dotspacemacs/user-config ()
